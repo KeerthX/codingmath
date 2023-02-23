cbf8eac546180851795d2ca7a0bbac69aa99eb2c
keerthx <officialprakeerthjm@gmail.com>
2023-02-23 21:03:57 +0530

    Added Plot function to the conjecture

--- collatz_conjecture(3n+1).py
+++ collatz_conjecture(3n+1).py
@@ -1,11 +1,24 @@
-def conj_collatz(n):
-    n = int(n)
+sequence = [0]
+import matplotlib.pyplot as plt
+
+def conj_collatz(n): #This functions gives the values of the collatz conjecture 
     while n!= 1:
         print(n)
         if (n % 2 == 0):
             n = n //2
         else:
             n = n*3 + 1
-    print(1)
+        sequence.append(n)
+    print(sequence)
+    return sequence
 
+def plot_collatz(n): #This functions gives the plots of the collatz conjecture 
+    conj_collatz(n)
+    x = range(len(sequence))
+    plt.plot(x, sequence)
+    plt.show()
+
+n = 30 #Enter the number here 
+conj_collatz(n)
+plot_collatz(n)
 
